c4ec409d3acfa3d448ba79188f4721d023ca8615
kayrosama <kayrosama@gmail.com>
2025-09-06 20:42:51 -0600

    First Commit

---
+++ core_auth/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework import status
+from core_auth.utils import is_token_valid
+
+def token_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        token = request.COOKIES.get('auth_token')
+        if not is_token_valid(token):
+            return redirect('login')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+def role_permission(required_roles, allow_self=False, restrict_role_change=True):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(self, request, *args, **kwargs):
+            user = request.user
+            target_user_id = request.data.get('id') or kwargs.get('pk')
+
+            if user.role not in required_roles:
+                return Response({'detail': 'Acceso denegado por rol'}, status=status.HTTP_403_FORBIDDEN)
+
+            if not allow_self and str(user.id) == str(target_user_id):
+                return Response({'detail': 'No puedes modificarte a ti mismo'}, status=status.HTTP_403_FORBIDDEN)
+
+            if restrict_role_change and str(user.id) == str(target_user_id) and 'role' in request.data:
+                return Response({'detail': 'No puedes modificar tu propio rol'}, status=status.HTTP_403_FORBIDDEN)
+
+            return view_func(self, request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
